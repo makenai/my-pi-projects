3b935f479817e4a6cae73e2ad67a65a54384b25d
Pawel Szymczykowski <makenai@gmail.com>
2012-12-17 02:24:03 +0000

    Modified zoltar.py

--- zoltar/zoltar.py
+++ zoltar/zoltar.py
@@ -44,8 +44,8 @@ BUTTON = 23
 
 header = Image.open('assets/receiptheader.jpg')
 def print_header():
-	# print receipt header
-	printer.print_bitmap(list(header.getdata()), 384, 324, True)
+	"print receipt header"
+	#printer.print_bitmap(list(header.getdata()), 384, 324, True)
 
 class zoltar :
     screen = None
